eeeee1b4a7ba32df4a5166d270fe21b48e1294ad
Liam Collod <lcollod@gmail.com>
2022-11-19 10:37:02 +0100

    [obs] changed CAT generation to pre-generated
    now all possible matrix combination are pre-generated

--- dev/scripts/hlsl-colorspace-generation.py
+++ dev/scripts/hlsl-colorspace-generation.py
@@ -1,12 +1,21 @@
 """
 Using colour & python to generate HLSL code for colorspace conversions.
 """
+import itertools
+import dataclasses
 from typing import Optional, List
 
 import colour
 import numpy
 
 ROUND_THRESHOLD = 9
+
+
+def slugify(string: str) -> str:
+    """
+    Convert to code variable compatible name.
+    """
+    return colour.utilities.slugify(string).replace("-", "")
 
 
 def convert3x3MatrixToHlslStr(matrix: numpy.ndarray) -> str:
@@ -26,43 +35,6 @@ def convert3x3MatrixToHlslStr(matrix: numpy.ndarray) -> str:
     )
 
 
-def processWhitepoint(whitepoint: numpy.ndarray, whitepointName: str) -> str:
-    """
-
-    Args:
-        whitepoint:
-        whitepointName:
-
-    Returns:
-        valid HLSL code snippet
-    """
-    array = colour.xy_to_XYZ(whitepoint)
-    whitepointName = whitepointName.replace("-", "")
-    return (
-        f"#define whitepoint_{whitepointName} "
-        f"float3({round(array[0], ROUND_THRESHOLD)}, {round(array[1], ROUND_THRESHOLD)}, {round(array[2], ROUND_THRESHOLD)})"
-    )
-
-
-def processColorspaceMatrix(colorspace: colour.RGB_Colourspace) -> str:
-    """
-
-    Args:
-        colorspace:
-
-    Returns:
-        valid HLSL code snippet
-    """
-    out_str = ""
-    out_str += f"// {colorspace.name}\n"
-    colorspaceName = colour.utilities.slugify(colorspace.name).replace("-", "")
-    matrix = colorspace.matrix_RGB_to_XYZ
-    out_str += f"#define matrix_{colorspaceName}_to_XYZ float3x3({convert3x3MatrixToHlslStr(matrix)})\n"
-    matrix = colorspace.matrix_XYZ_to_RGB
-    out_str += f"#define matrix_{colorspaceName}_from_XYZ float3x3({convert3x3MatrixToHlslStr(matrix)})\n"
-    return out_str
-
-
 def generateCommentHeader(title: str, description: Optional[str] = None) -> str:
     """
     Generate an HLSL comment block to summarize the code that can be found under.
@@ -79,12 +51,89 @@ def generateCommentHeader(title: str, description: Optional[str] = None) -> str:
     return out_str
 
 
+@dataclasses.dataclass
+class Whitepoint:
+
+    name: str
+    coordinates: numpy.ndarray
+    id: int = dataclasses.field(default_factory=itertools.count().__next__, init=False)
+
+    def __post_init__(self):
+        self.safe_name = slugify(self.name)
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+@dataclasses.dataclass
+class Cat:
+
+    name: str
+    id: int = dataclasses.field(default_factory=itertools.count().__next__, init=False)
+
+    def __post_init__(self):
+        self.safe_name = slugify(self.name)
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+@dataclasses.dataclass
+class HlslVariable:
+
+    name: str
+    definition: str
+
+
+def processCat(
+    whitepoint_source: Whitepoint,
+    whitepoint_target: Whitepoint,
+    cat: Cat,
+) -> HlslVariable:
+
+    matrix_cat = colour.adaptation.matrix_chromatic_adaptation_VonKries(
+        colour.xy_to_XYZ(whitepoint_source.coordinates),
+        colour.xy_to_XYZ(whitepoint_target.coordinates),
+        cat.name,
+    )
+
+    variable_name = f"matrix_cat_{cat.safe_name}_{whitepoint_source.safe_name}_to_{whitepoint_target.safe_name}"
+    variable_def = (
+        f"#define {variable_name} float3x3({convert3x3MatrixToHlslStr(matrix_cat)})\n"
+    )
+    return HlslVariable(variable_name, variable_def)
+
+
+def processColorspaceMatrix(colorspace: colour.RGB_Colourspace) -> str:
+    """
+
+    Args:
+        colorspace:
+
+    Returns:
+        valid HLSL code snippet
+    """
+    out_str = ""
+    out_str += f"// {colorspace.name}\n"
+    colorspaceName = slugify(colorspace.name)
+    matrix = colorspace.matrix_RGB_to_XYZ
+    out_str += f"#define matrix_{colorspaceName}_to_XYZ float3x3({convert3x3MatrixToHlslStr(matrix)})\n"
+    matrix = colorspace.matrix_XYZ_to_RGB
+    out_str += f"#define matrix_{colorspaceName}_from_XYZ float3x3({convert3x3MatrixToHlslStr(matrix)})\n"
+    return out_str
+
+
 class Generator:
     """
     Generate HLSL code as a string following the given input attributes.
     """
 
-    def __init__(self, colorspaceNames, whitepointNames, catNames):
+    def __init__(
+        self,
+        colorspaceNames: list[str],
+        whitepointNames: list[str],
+        catNames: list[str],
+    ):
 
         self.colorspaceNames = colorspaceNames
         self.whitepointNames = whitepointNames
@@ -99,31 +148,12 @@ class Generator:
             "CIE 1931 2 Degree Standard Observer"
         ]
 
-        self.whitepointToProcessDict: dict[str, numpy.ndarray] = {}
-        """
-        {"whitepoint Name": whitepoint coordinates as [x, y]}
-        """
-        for whitepoint_name in whitepointNames:
-            self.whitepointToProcessDict[whitepoint_name] = illuminant1931[
-                whitepoint_name
-            ]
-        # append whitepoint from processed colorspaces
-        for colorspace in self.colorspaceToProcessList:
+        self.whitepointToProcessList: list[Whitepoint] = [
+            Whitepoint(whitepoint_name, illuminant1931[whitepoint_name])
+            for whitepoint_name in whitepointNames
+        ]
 
-            if self.whitepointToProcessDict.get(colorspace.whitepoint_name) is not None:
-                continue
-
-            self.whitepointToProcessDict[
-                colorspace.whitepoint_name
-            ] = colorspace.whitepoint
-
-        self.catToProcessDict: dict[str, numpy.ndarray] = {
-            catName: colour.CHROMATIC_ADAPTATION_TRANSFORMS[catName]
-            for catName in self.catNames
-        }
-        """
-        {"CAT Name": CAT 3x3 matrix}
-        """
+        self.catToProcessList: list[Cat] = [Cat(cat_name) for cat_name in self.catNames]
 
     def generateCode(self) -> str:
         """
@@ -132,10 +162,9 @@ class Generator:
         """
 
         str_colorspace = self._generateMatricesBlock()
-        str_whitepoint = self._generateWhitepointsBlock()
         str_cat = self._generateCatBlock()
 
-        return f"{str_cat}\n\n{str_whitepoint}\n\n{str_colorspace}"
+        return f"{str_cat}\n\n{str_colorspace}"
 
     def _generateMatricesBlock(self) -> str:
 
@@ -148,24 +177,54 @@ class Generator:
 
         return out_str
 
-    def _generateWhitepointsBlock(self) -> str:
-
-        out_str = generateCommentHeader("Whitepoints", "xy values converted to XYZ")
-        out_str += "\n"
-
-        for whitepoint_name, whitepoint in self.whitepointToProcessDict.items():
-            out_str += processWhitepoint(whitepoint, whitepoint_name) + "\n"
-
-        return out_str
-
     def _generateCatBlock(self) -> str:
 
         out_str = generateCommentHeader("Chromatic Adaptation Transforms")
         out_str += "\n"
 
-        for cat_name, cat in self.catToProcessDict.items():
-            cat_name_slug = colour.utilities.slugify(cat_name).replace("-", "")
-            out_str += f"#define matrix_cat_{cat_name_slug} float3x3({convert3x3MatrixToHlslStr(cat)})\n"
+        whitepoint_combinaison_list = list(
+            itertools.product(self.whitepointToProcessList, repeat=2)
+        )
+
+        cat_variable_dict = dict()
+
+        for cat in self.catToProcessList:
+
+            for whitepoint_combinaison in whitepoint_combinaison_list:
+
+                whitepoint_source = whitepoint_combinaison[0]
+                whitepoint_target = whitepoint_combinaison[1]
+
+                if whitepoint_source == whitepoint_target:
+                    continue
+
+                cat_variable = processCat(whitepoint_source, whitepoint_target, cat)
+                generated_id = (cat.id, whitepoint_source.id, whitepoint_target.id)
+                cat_variable_dict[generated_id] = cat_variable
+
+                out_str += cat_variable.definition
+                continue
+
+        out_str += "\n"
+
+        for cat in self.catToProcessList:
+            out_str += f"uniform int catid_{cat.safe_name} = {cat.id};\n"
+
+        out_str += "\n"
+
+        for whitepoint in self.whitepointToProcessList:
+            out_str += (
+                f"uniform int whitepointid_{whitepoint.safe_name} = {whitepoint.id};\n"
+            )
+
+        out_str += "\n\n"
+        out_str += "float3x3 getChromaticAdaptationTransformMatrix(int cat_name, int whitepoint_source, int whitepoint_target){\n"
+
+        for cat_variable_id, cat_variable in cat_variable_dict.items():
+            out_str += f"    if (cat_name == {cat_variable_id[0]} && whitepoint_source == {cat_variable_id[1]} && whitepoint_target == {cat_variable_id[2]})"
+            out_str += f" return {cat_variable.name};\n"
+
+        out_str += "}"
 
         return out_str
 
@@ -182,6 +241,8 @@ def main():
         ],
         whitepointNames=[
             "D60",
+            "D65",
+            "DCI-P3",
         ],
         catNames=[
             "XYZ Scaling",
--- obs/obs-script/colorspace.hlsl
+++ obs/obs-script/colorspace.hlsl
@@ -114,25 +114,125 @@ float3 cctf_encoding_bt2020(float3 color){return color;}  // TODO
 Chromatic Adaptation Transforms
 -------------------------------------------------------------------------------- */
 
-#define matrix_cat_xyzscaling float3x3(\
-  1.0, 0.0, 0.0,\
+#define matrix_cat_xyzscaling_d60_to_d65 float3x3(\
+  0.997749312, 0.0, 0.0,\
   0.0, 1.0, 0.0,\
-  0.0, 0.0, 1.0\
+  0.0, 0.0, 1.079011464\
 )
-#define matrix_cat_bradford float3x3(\
-  0.8951, 0.2664, -0.1614,\
-  -0.7502, 1.7135, 0.0367,\
-  0.0389, -0.0685, 1.0296\
+#define matrix_cat_xyzscaling_d60_to_dcip3 float3x3(\
+  0.939100313, 0.0, 0.0,\
+  0.0, 1.0, 0.0,\
+  0.0, 0.0, 0.945611705\
 )
-#define matrix_cat_cat02 float3x3(\
-  0.7328, 0.4296, -0.1624,\
-  -0.7036, 1.6975, 0.0061,\
-  0.003, 0.0136, 0.9834\
+#define matrix_cat_xyzscaling_d65_to_d60 float3x3(\
+  1.002255765, 0.0, 0.0,\
+  0.0, 1.0, 0.0,\
+  0.0, 0.0, 0.926774212\
 )
-#define matrix_cat_vonkries float3x3(\
-  0.40024, 0.7076, -0.08081,\
-  -0.2263, 1.16532, 0.0457,\
-  0.0, 0.0, 0.91822\
+#define matrix_cat_xyzscaling_d65_to_dcip3 float3x3(\
+  0.941218703, 0.0, 0.0,\
+  0.0, 1.0, 0.0,\
+  0.0, 0.0, 0.876368543\
+)
+#define matrix_cat_xyzscaling_dcip3_to_d60 float3x3(\
+  1.064848969, 0.0, 0.0,\
+  0.0, 1.0, 0.0,\
+  0.0, 0.0, 1.05751652\
+)
+#define matrix_cat_xyzscaling_dcip3_to_d65 float3x3(\
+  1.062452326, 0.0, 0.0,\
+  0.0, 1.0, 0.0,\
+  0.0, 0.0, 1.141072449\
+)
+#define matrix_cat_bradford_d60_to_d65 float3x3(\
+  0.987323904, -0.006062169, 0.015845876,\
+  -0.007531959, 1.001832321, 0.005293338,\
+  0.00305302, -0.005064257, 1.081147527\
+)
+#define matrix_cat_bradford_d60_to_dcip3 float3x3(\
+  0.964265012, -0.021300233, -0.002647012,\
+  -0.033122492, 1.030598686, 0.000944999,\
+  -0.003057139, 0.006720893, 0.941838177\
+)
+#define matrix_cat_bradford_d65_to_d60 float3x3(\
+  1.012931026, 0.006054132, -0.0148757,\
+  0.007630325, 0.998191932, -0.004999018,\
+  -0.002824644, 0.004658585, 0.924961741\
+)
+#define matrix_cat_bradford_d65_to_dcip3 float3x3(\
+  0.976578897, -0.015436265, -0.016686022,\
+  -0.025689666, 1.028539168, -0.003785174,\
+  -0.005705746, 0.011077866, 0.871176159\
+)
+#define matrix_cat_bradford_dcip3_to_d60 float3x3(\
+  1.037804611, 0.021430283, 0.002895221,\
+  0.033351213, 0.971004834, -0.00088053,\
+  0.003130647, -0.006859463, 1.061769201\
+)
+#define matrix_cat_bradford_dcip3_to_d65 float3x3(\
+  1.024496728, 0.015163541, 0.019688522,\
+  0.025612193, 0.972586306, 0.004716352,\
+  0.006384231, -0.012268083, 1.147942445\
+)
+#define matrix_cat_cat02_d60_to_d65 float3x3(\
+  0.988317991, -0.007816256, 0.016645551,\
+  -0.00564353, 0.998686699, 0.00662762,\
+  0.00035077, 0.001116791, 1.077573915\
+)
+#define matrix_cat_cat02_d60_to_dcip3 float3x3(\
+  0.979655622, -0.035093475, -0.003506859,\
+  -0.024670911, 1.024631944, -0.001120007,\
+  0.000239512, -0.000975875, 0.946352523\
+)
+#define matrix_cat_cat02_d65_to_d60 float3x3(\
+  1.011870978, 0.007936977, -0.015679437,\
+  0.005720259, 1.001366784, -0.00624727,\
+  -0.000335311, -0.001040394, 0.928022164\
+)
+#define matrix_cat_cat02_d65_to_dcip3 float3x3(\
+  0.991085525, -0.027362287, -0.018395654,\
+  -0.019102244, 1.025837747, -0.007053718,\
+  -8.0549e-05, -0.001959887, 0.878238458\
+)
+#define matrix_cat_cat02_dcip3_to_d60 float3x3(\
+  1.021647216, 0.034994893, 0.003827292,\
+  0.024598791, 0.976803896, 0.001247201,\
+  -0.000233203, 0.00099842, 1.056688999\
+)
+#define matrix_cat_cat02_dcip3_to_d65 float3x3(\
+  1.009516172, 0.026967753, 0.021362003,\
+  0.018799243, 0.97533018, 0.008227297,\
+  0.000134542, 0.002179032, 1.138663236\
+)
+#define matrix_cat_vonkries_d60_to_d65 float3x3(\
+  0.995663132, -0.014998519, 0.016829124,\
+  -0.001647481, 1.001234102, 0.000332195,\
+  0.0, 0.0, 1.079011464\
+)
+#define matrix_cat_vonkries_d60_to_dcip3 float3x3(\
+  0.9888443, -0.038575341, -0.0087295,\
+  -0.004237228, 1.003172521, 0.000855893,\
+  0.0, 0.0, 0.945611705\
+)
+#define matrix_cat_vonkries_d65_to_d60 float3x3(\
+  1.004380654, 0.015045655, -0.015669755,\
+  0.001652658, 0.998792176, -0.000333274,\
+  0.0, 0.0, 0.926774212\
+)
+#define matrix_cat_vonkries_d65_to_dcip3 float3x3(\
+  0.993112333, -0.023650939, -0.023572367,\
+  -0.002597888, 1.001897113, 0.000525285,\
+  0.0, 0.0, 0.876368543\
+)
+#define matrix_cat_vonkries_dcip3_to_d60 float3x3(\
+  1.011448214, 0.038893569, 0.009302073,\
+  0.004272183, 0.997001792, -0.000862968,\
+  0.0, 0.0, 1.05751652\
+)
+#define matrix_cat_vonkries_dcip3_to_d65 float3x3(\
+  1.00699762, 0.023771342, 0.027071751,\
+  0.002611113, 0.998168118, -0.000528057,\
+  0.0, 0.0, 1.141072449\
 )
 
 uniform int catid_xyzscaling = 0;
@@ -140,43 +240,37 @@ uniform int catid_bradford = 1;
 uniform int catid_cat02 = 2;
 uniform int catid_vonkries = 3;
 
+uniform int whitepointid_d60 = 0;
+uniform int whitepointid_d65 = 1;
+uniform int whitepointid_dcip3 = 2;
 
-float3x3 matrix_chromatic_adaptation_VonKries(float3 whitepoint_source, float3 whitepoint_target, int cat_id){
 
-  float3x3 cat_matrix;
-  if (cat_id == catid_xyzscaling) cat_matrix = matrix_cat_xyzscaling;
-  if (cat_id == catid_bradford) cat_matrix = matrix_cat_bradford;
-  if (cat_id == catid_cat02) cat_matrix = matrix_cat_cat02;
-  if (cat_id == catid_vonkries) cat_matrix = matrix_cat_vonkries;
-
-  whitepoint_source = applyMatrix(whitepoint_source, cat_matrix);
-  whitepoint_target = applyMatrix(whitepoint_target, cat_matrix);
-
-  whitepoint_target = whitepoint_target / whitepoint_source;
-
-  float3x3 cat_matrix_final = {
-    whitepoint_target[0], 0.0, 0.0,
-    0.0, whitepoint_target[1], 0.0,
-    0.0, 0.0, whitepoint_target[2]
-  };
-
-  cat_matrix_final = mul(cat_matrix, cat_matrix_final);
-  cat_matrix_final = mul(cat_matrix_final, cat_matrix);
-
-  return cat_matrix_final;
+float3x3 getChromaticAdaptationTransformMatrix(int cat_name, int whitepoint_source, int whitepoint_target){
+    if (cat_name == 0 && whitepoint_source == 0 && whitepoint_target == 1) return matrix_cat_xyzscaling_d60_to_d65;
+    if (cat_name == 0 && whitepoint_source == 0 && whitepoint_target == 2) return matrix_cat_xyzscaling_d60_to_dcip3;
+    if (cat_name == 0 && whitepoint_source == 1 && whitepoint_target == 0) return matrix_cat_xyzscaling_d65_to_d60;
+    if (cat_name == 0 && whitepoint_source == 1 && whitepoint_target == 2) return matrix_cat_xyzscaling_d65_to_dcip3;
+    if (cat_name == 0 && whitepoint_source == 2 && whitepoint_target == 0) return matrix_cat_xyzscaling_dcip3_to_d60;
+    if (cat_name == 0 && whitepoint_source == 2 && whitepoint_target == 1) return matrix_cat_xyzscaling_dcip3_to_d65;
+    if (cat_name == 1 && whitepoint_source == 0 && whitepoint_target == 1) return matrix_cat_bradford_d60_to_d65;
+    if (cat_name == 1 && whitepoint_source == 0 && whitepoint_target == 2) return matrix_cat_bradford_d60_to_dcip3;
+    if (cat_name == 1 && whitepoint_source == 1 && whitepoint_target == 0) return matrix_cat_bradford_d65_to_d60;
+    if (cat_name == 1 && whitepoint_source == 1 && whitepoint_target == 2) return matrix_cat_bradford_d65_to_dcip3;
+    if (cat_name == 1 && whitepoint_source == 2 && whitepoint_target == 0) return matrix_cat_bradford_dcip3_to_d60;
+    if (cat_name == 1 && whitepoint_source == 2 && whitepoint_target == 1) return matrix_cat_bradford_dcip3_to_d65;
+    if (cat_name == 2 && whitepoint_source == 0 && whitepoint_target == 1) return matrix_cat_cat02_d60_to_d65;
+    if (cat_name == 2 && whitepoint_source == 0 && whitepoint_target == 2) return matrix_cat_cat02_d60_to_dcip3;
+    if (cat_name == 2 && whitepoint_source == 1 && whitepoint_target == 0) return matrix_cat_cat02_d65_to_d60;
+    if (cat_name == 2 && whitepoint_source == 1 && whitepoint_target == 2) return matrix_cat_cat02_d65_to_dcip3;
+    if (cat_name == 2 && whitepoint_source == 2 && whitepoint_target == 0) return matrix_cat_cat02_dcip3_to_d60;
+    if (cat_name == 2 && whitepoint_source == 2 && whitepoint_target == 1) return matrix_cat_cat02_dcip3_to_d65;
+    if (cat_name == 3 && whitepoint_source == 0 && whitepoint_target == 1) return matrix_cat_vonkries_d60_to_d65;
+    if (cat_name == 3 && whitepoint_source == 0 && whitepoint_target == 2) return matrix_cat_vonkries_d60_to_dcip3;
+    if (cat_name == 3 && whitepoint_source == 1 && whitepoint_target == 0) return matrix_cat_vonkries_d65_to_d60;
+    if (cat_name == 3 && whitepoint_source == 1 && whitepoint_target == 2) return matrix_cat_vonkries_d65_to_dcip3;
+    if (cat_name == 3 && whitepoint_source == 2 && whitepoint_target == 0) return matrix_cat_vonkries_dcip3_to_d60;
+    if (cat_name == 3 && whitepoint_source == 2 && whitepoint_target == 1) return matrix_cat_vonkries_dcip3_to_d65;
 }
-
-
-/* --------------------------------------------------------------------------------
-Whitepoints
-
-xy values converted to XYZ
--------------------------------------------------------------------------------- */
-
-#define whitepoint_D60 float3(0.952599932, 1.0, 1.009310639)
-#define whitepoint_D65 float3(0.950455927, 1.0, 1.089057751)
-#define whitepoint_DCIP3 float3(0.894586895, 1.0, 0.954415954)
-
 
 /* --------------------------------------------------------------------------------
 Matrices
@@ -270,7 +364,7 @@ float3 convertColorspaceToColorspace(float3 color, int sourceColorspaceId, int t
     float3 whitepoint_target;
 
     if (sourceColorspaceId == colorspaceid_sRGB_Display_EOTF){
-        whitepoint_source = whitepoint_D65;
+        whitepoint_source = whitepointid_d65;
         color = cctf_decoding_sRGB(color);
         color = applyMatrix(color, matrix_srgb_to_XYZ);
     }
